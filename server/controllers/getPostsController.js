ba0b86c2ff7516822eb34ae3e623c63888009df8
amasri88 <amran.man99@gmail.com>
2022-03-27 21:34:10 +0300

    Creating navbar relats #11

--- server/controllers/getPostsController.js
+++ server/controllers/getPostsController.js
@@ -3,9 +3,8 @@ const { getPostsQuery } = require('../database/queires');
 const getPosts = (req, res)=>{
     getPostsQuery()
       .then(data => res.json(data.rows))
-
       .catch(err => res.status(500).json({messege: 'Internal server error!'}))
-      // .catch(err => next(err))
+      
 };
 
 

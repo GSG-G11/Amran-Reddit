7e737fa8da2c0d2210d092931d08d42b2d72b1c2
amasri88 <amran.man99@gmail.com>
2022-03-31 22:33:53 +0300

    fix many of code

--- server/validation/signupSchema.js
+++ server/validation/signupSchema.js
@@ -2,8 +2,8 @@ const joi = require('joi');
 
 const signupSchema = joi.object({
     username: joi.string().min(3).max(30).required(),
-    email: joi.string().email().required(),
-    password: joi.string().alphanum().min(7).required()
+    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }).required(),
+    password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required()
 })
 
 
